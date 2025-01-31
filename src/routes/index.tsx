9fc2bf9f8d5818847a6af14c6aef733b8b3c7fad
TheComputerM <mudit.somani00@gmail.com>
2025-01-31 14:14:38 +0530

    feat: add logging

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,43 @@
 import { onUrl, start } from "@fabianlars/tauri-plugin-oauth";
+import { useState } from "react";
 import { logtoClient } from "../lib/logto";
 
 const LoginButton = () => {
+	const [logs, setLogs] = useState<string[]>([]);
+
 	async function handleSignin() {
 		const port = await start();
-		console.log(port);
+		setLogs((logs) => [...logs, `Server started on port ${port}`]);
 		await logtoClient.signIn(`http://localhost:${port}`);
+
 		await onUrl(async (url) => {
+			setLogs((logs) => [...logs, `Callback received: ${url}`]);
+
 			await logtoClient.handleSignInCallback(url);
 			if (await logtoClient.isAuthenticated()) {
+				const accessToken = await logtoClient.getAccessToken();
+				setLogs((logs) => [...logs, `Access Token: ${accessToken}`]);
+
 				alert("Authentication successful!");
+			} else {
+				alert("Authentication failed!");
 			}
 		});
 	}
 
-	return <button type="button" onClick={handleSignin}>Login</button>;
+	return (
+		<div>
+			<button type="button" onClick={handleSignin}>
+				Login
+			</button>
+			<ul>
+				{logs.map((log, i) => (
+					// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+					<li key={i}>{log}</li>
+				))}
+			</ul>
+		</div>
+	);
 };
 
 export const HomePage = () => {
