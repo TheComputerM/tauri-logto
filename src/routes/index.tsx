c97b154def661ee545cccef65e2e130799643039
TheComputerM <mudit.somani00@gmail.com>
2025-01-31 13:06:44 +0530

    feat: add tauri oauth plugin

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,20 @@
+import { onUrl, start } from "@fabianlars/tauri-plugin-oauth";
+import { logtoClient } from "../lib/logto";
+
 const LoginButton = () => {
-	return <button type="button">Login</button>;
+	async function handleSignin() {
+		const port = await start();
+		console.log(port);
+		await logtoClient.signIn(`http://localhost:${port}`);
+		await onUrl(async (url) => {
+			await logtoClient.handleSignInCallback(url);
+			if (await logtoClient.isAuthenticated()) {
+				alert("Authentication successful!");
+			}
+		});
+	}
+
+	return <button type="button" onClick={handleSignin}>Login</button>;
 };
 
 export const HomePage = () => {
