6ad8e05492de36e80376329c2fb2a272a57a9536
TheComputerM <mudit.somani00@gmail.com>
2025-02-01 12:44:12 +0530

    chore: remove oauth plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,7 +2,6 @@
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .plugin(tauri_plugin_oauth::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,68 +1,10 @@
-import { onUrl, start } from "@fabianlars/tauri-plugin-oauth";
-import { logtoClient } from "../lib/logto";
-import { isTauri } from "../lib/detect-tauri";
-import { useEffect, useState } from "react";
-
-const LoginButton = () => {
-	async function handleTauriLogin() {
-		const port = await start();
-		await logtoClient.signIn(`http://localhost:${port}`);
-
-		await onUrl(async (url) => {
-			await logtoClient.handleSignInCallback(url);
-			if (await logtoClient.isAuthenticated()) {
-				alert("Authentication successful!");
-			} else {
-				alert("Authentication failed!");
-			}
-		});
-	}
-
-	async function handleWebLogin() {
-		// also add this to your logto application's valid redirect uri's
-		await logtoClient.signIn(new URL(`${window.location.origin}/callback`));
-	}
-
-	return (
-		<button type="button" onClick={isTauri ? handleTauriLogin : handleWebLogin}>
-			Login
-		</button>
-	);
-};
-
-const Status = () => {
-	const [properties, setProperties] = useState<Record<string, string>>({});
-
-	async function onMount() {
-		setProperties({
-			"Logged In": (await logtoClient.isAuthenticated()) ? "true" : "false",
-			"Access Token": await logtoClient.getAccessToken(),
-		});
-	}
-	useEffect(() => {
-		onMount();
-	});
-
-	return (
-		<ul>
-			<li>Mode: {isTauri ? "Tauri" : "Web"}</li>
-			{Object.entries(properties).map(([key, value]) => (
-				<li key={key}>
-					{key}: {value}
-				</li>
-			))}
-		</ul>
-	);
-};
-
 export const HomePage = () => {
 	return (
 		<main>
 			<br />
 			<div className="container">
 				<h1>Tauri + Logto OAuth2</h1>
-				<LoginButton />
-				<Status />
+				
 			</div>
 		</main>
 	);
